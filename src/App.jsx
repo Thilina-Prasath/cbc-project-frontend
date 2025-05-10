49b1b93ca8a8a9b7a4bfaf6b81edd0be638c009d
Thilina-Prasath <2022to1546@stu.cmb.ac.lk>
2025-05-11 01:29:38 +0530

    Day 10

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
-import './App.css'
-import Header from './components/header'
-import ProductCard from './components/productCard'
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import HomePage from "./pages/home"
+import LoginPage from "./pages/login"
+import SignupPage from "./pages/signup"
+import AdminPage from "./pages/adminPage"
 
 function App() {
 
   return (
-    <>
-      <Header/>
-      <ProductCard name ="businesslaptop" description="fjkdndkdjkff" price ="2300" picture="https://picsum.photos/id/2/200/300"/>
-      <ProductCard name ="gaming laptop" description="fjkdndkdjkff" price ="2300" picture="https://picsum.photos/id/2/200/300"/>
+    <BrowserRouter>
+    <div>
+      <Routes path="/*">         {/*Dynamic kotuwk hdnn gththe routes.ek athule thmi route thiyenne*/}
+          <Route path="/" element={<HomePage/>}/>       {/*Route ekk kiynne ek awsthwkdi penn on deyk*/}
+          <Route path='/login' element={<LoginPage/>}/>
+          <Route path="/signup" element={<SignupPage/>}/>
+          <Route path='/admin/*' element={<AdminPage/>}/>      {/*/admin/* admin ekth ekk adminge psse mkk hri deyk ekthu wenw nm /* mehm dno*/}
+          <Route path='/*' element={<h1>404 Not Found</h1>}/>      {/*kisim url ekkt gelpenne nethi ekkt awm */}
 
-    </>
+      </Routes>
+    </div>
+    </BrowserRouter>
   )
 }
 
---
+++ src/pages/login.jsx
@@ -0,0 +1,10 @@
+export default function LoginPage(){
+    return(
+        <div className="bg-red-500">   
+                    <h1 className="text-blue-950">hiiii</h1>         {/*tailwind css eke nthi mkk hri api denw nm manualy [] athule dnn plwn*/}
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti, cupiditate.</p>
+                                <button className="buy now">login</button>
+
+        </div>
+    )
+}
