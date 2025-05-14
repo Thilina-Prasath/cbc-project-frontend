99251f727ec269d5ab55343737563e7502122783
Thilina-Prasath <2022to1546@stu.cmb.ac.lk>
2025-05-14 14:26:31 +0530

    day 11

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,20 @@ import HomePage from "./pages/home"
 import LoginPage from "./pages/login"
 import SignupPage from "./pages/signup"
 import AdminPage from "./pages/adminPage"
+import TestPage from "./pages/testPage"
+import { Toaster } from "react-hot-toast"
 
 function App() {
 
   return (
     <BrowserRouter>
     <div>
+      <Toaster position="top-center"/>  {/*notification ek enn on position ek.*/}
       <Routes path="/*">         {/*Dynamic kotuwk hdnn gththe routes.ek athule thmi route thiyenne*/}
           <Route path="/" element={<HomePage/>}/>       {/*Route ekk kiynne ek awsthwkdi penn on deyk*/}
           <Route path='/login' element={<LoginPage/>}/>
           <Route path="/signup" element={<SignupPage/>}/>
+          <Route path="/testing" element={<TestPage/>}/>
           <Route path='/admin/*' element={<AdminPage/>}/>      {/*/admin/* admin ekth ekk adminge psse mkk hri deyk ekthu wenw nm /* mehm dno*/}
           <Route path='/*' element={<h1>404 Not Found</h1>}/>      {/*kisim url ekkt gelpenne nethi ekkt awm */}
 
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,10 +1,61 @@
-export default function LoginPage(){
-    return(
-        <div className="bg-red-500">   
-                    <h1 className="text-blue-950">hiiii</h1>         {/*tailwind css eke nthi mkk hri api denw nm manualy [] athule dnn plwn*/}
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti, cupiditate.</p>
-                                <button className="buy now">login</button>
+import axios from "axios"
+import { useState } from "react"
+import toast from "react-hot-toast"
 
+export default function LoginPage(){
+    const [email,setEmail] = useState("")
+    const [password,setPassword] = useState("")
+
+    async function handleLogin(){
+        //console.log(email)    /*mekedi wenne login ek ebuwm email eke saha password eke http request ekk backend ekt yn ek*/
+        //console.log(password)
+ 
+        try{
+            const response = await axios.post("http://localhost:3000/api/users/login",{         //awit ek dnne meken lbenne promiss ekk nis,axios.post ekn wenne me thiyen url ek ynn on then
+            email:email,           //ywnn on json ek
+            password:password
+        })
+
+        toast.success("Login successful");   //notification lesiyen penn plwn
+        console.log(response.data)
+      
+        }catch(e){
+            toast.error(e.response.data.message);
+        }
+        
+    }
+    return(
+        <div className="w-full h-screen bg-[url('/login.jpg')] bg-center bg-cover flex justify-evenly items-center ">  {/*back ground ekk add krno*/}
+            <div className="w-[50%] h-full ">   {/*screen ekdekt bedno*/}
+
+            </div>
+            <div className="w-[50%] h-full flex justify-center items-center "> {/*screen ekdekt bedno*/}
+                <div className="w-[500px] h-[600px] backdrop-blur-md rounded-[20px] shadow-xl flex flex-col justify-center items-center"> {/*aluth ekk dl ek blue krno*/}
+                    <input onChange={
+                        (e)=>{    {/*e wlin krnne api krn siylum wens km penno.e kiynne email ek wens klm klin mail ek aluth mail ek saha time ek penno*/} 
+                            setEmail(e.target.value)                        
+                        }
+                    }
+
+                    value={email}
+
+                    className="w-[300px] h-[50px] border border-[#c3efe9] rounded-[20px] my-[20px]"/>
+                    <input  onChange={
+                        (e)=>{    {/*e wlin krnne api krn siylum wens km penno.e kiynne email ek wens klm klin mail ek aluth mail ek saha time ek penno*/} 
+                            setPassword(e.target.value)                        
+                        }
+                    }
+
+                    value={password}
+                    
+                    type="password" className="w-[300px] h-[50px] border border-[#c3efe9] rounded-[20px] my-[20px]" />
+                    <button onClick={handleLogin}className="w-[300px] h-[50px] bg-[#c3efe9] rounded-[20px] my-[20px] text-white cursor-pointer">login</button>
+
+
+
+                </div>
+
+            </div>
         </div>
     )
 }
